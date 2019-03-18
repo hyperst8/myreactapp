619fa7759fd1529cfe0a167f42479caa587af154
Huy Nguyen <Huy.Nguyen@asker.kommune.no>
2019-03-18 15:03:02 +0100

    Updates

---
+++ precache-manifest.16e75ca114f903dddf76113490165b54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a1bed599305e87249f",
+    "url": "/myreactapp/static/js/runtime~main.3dcc1287.js"
+  },
+  {
+    "revision": "41f54c5472cc77064520",
+    "url": "/myreactapp/static/js/main.0bf834f3.chunk.js"
+  },
+  {
+    "revision": "73080c8c5313bf1fb476",
+    "url": "/myreactapp/static/js/2.d519b009.chunk.js"
+  },
+  {
+    "revision": "41f54c5472cc77064520",
+    "url": "/myreactapp/static/css/main.4aa14553.chunk.css"
+  },
+  {
+    "revision": "a934350efb26b5bff78a9cf67cd987a8",
+    "url": "/myreactapp/index.html"
+  }
+];
